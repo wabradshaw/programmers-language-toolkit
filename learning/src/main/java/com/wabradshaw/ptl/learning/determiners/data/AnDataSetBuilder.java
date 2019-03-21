9c536b4577b5e76ea6145dbc047ee96bb4d4e4bc
William Bradshaw <w.a.bradshaw@gmail.com>
2019-03-21 23:21:59 +0000

    Class for generating datasets from ngrams

---
+++ learning/src/main/java/com/wabradshaw/ptl/learning/determiners/data/AnDataSetBuilder.java
@@ -0,0 +1,144 @@
+package com.wabradshaw.ptl.learning.determiners.data;
+
+import com.google.common.io.Files;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Random;
+
+/**
+ * A class designed to load in huge ordered files containing usage counts of "a whatever" or "an whatever" and use them
+ * to create datasets suitable for machine learning. Dataset partitioning is random, so datasets may be slightly
+ * different sizes.
+ *
+ * Input data is based on Google N-Grams:
+ *   a/an word TAB year TAB match_count TAB volume_count NEWLINE
+ *
+ * Output entries are of the format:
+ *   word TAB ratio NEWLINE
+ * Where ratio is the percentage of ngrams using "an" rather than "a".
+ */
+public class AnDataSetBuilder {
+
+    private Random rng = new Random();
+
+    /**
+     * Creates a number of "a or an" dataset files that can be used to train a machine learning model.
+     *
+     * Input data is based on Google N-Grams:
+     *   a/an word TAB year TAB match_count TAB volume_count NEWLINE
+     *
+     * Output entries are of the format:
+     *   word TAB ratio NEWLINE
+     * Where ratio is the percentage of ngrams using "an" rather than "a".
+     *
+     * Output files are created by index (starting at 0.txt)
+     *
+     * @param aFile The file containing counts of ngrams that start "a ".
+     * @param anFile The file containing counts of ngrams that start "an ".
+     * @param outputDirectory The directory the dataset should be written into.
+     * @param datasets The number of datasets to be created. If this approaches or exceeds the number of entries,
+     *                 some datasets may be empty.
+     */
+    public void build(File aFile, File anFile, File outputDirectory, int datasets) throws IOException {
+        if(!outputDirectory.exists()){
+            outputDirectory.mkdir();
+        }
+
+        BufferedWriter[] fileWriters = new BufferedWriter[datasets];
+        for(int i = 0; i < datasets; i++){
+            fileWriters[i] = Files.newWriter(new File(outputDirectory.getPath() + "/" + i +".txt"),
+                                             Charset.defaultCharset());
+        }
+
+        LineIterator as = FileUtils.lineIterator(aFile);
+        LineIterator ans = FileUtils.lineIterator(anFile);
+        UsageCount nextA = as.hasNext() ? new UsageCount(as.next(), false) : null;
+        UsageCount nextAn = ans.hasNext() ? new UsageCount(ans.next(), true) : null;
+        UsageCount current = null;
+
+        if(nextA != null && nextA.before(nextAn)){
+            current = nextA;
+            nextA = as.hasNext() ? new UsageCount(as.next(), false) : null;
+        } else if (nextAn != null){
+            current = nextAn;
+            nextAn = ans.hasNext() ? new UsageCount(ans.next(), true) : null;
+        }
+
+        while(nextA != null || nextAn != null){
+            if(nextA != null && nextA.before(nextAn)){
+                if(nextA.matches(current)) {
+                    current.add(nextA);
+                    nextA = as.hasNext() ? new UsageCount(as.next(), false) : null;
+                } else {
+                    fileWriters[rng.nextInt(datasets)].write(current.toString());
+                    current = nextA;
+                }
+            } else {
+                if(nextAn.matches(current)) {
+                    current.add(nextAn);
+                    nextAn = ans.hasNext() ? new UsageCount(ans.next(), true) : null;
+                } else {
+                    fileWriters[rng.nextInt(datasets)].write(current.toString());
+                    current = nextAn;
+                }
+            }
+        }
+
+        if(current != null){
+            fileWriters[rng.nextInt(datasets)].write(current.toString());
+        }
+
+        for (BufferedWriter fileWriter : fileWriters) {
+            fileWriter.close();
+        }
+    }
+
+    /**
+     * Helper class to store information about how many times a word was prefaced with a or an.
+     */
+    private class UsageCount{
+        private final String word;
+        private int aCount = 0;
+        private int anCount = 0;
+
+        /**
+         * Creates a UsageCount by parsing the input string.
+         *
+         * @param contents The string containing the determiner-word ngram and its count
+         * @param an Whether or not it's from the an file
+         */
+        private UsageCount(String contents, boolean an) {
+            String[] split = contents.split("\t");
+            if(an) {
+                this.word = split[0].substring(3);
+                anCount = Integer.parseInt(split[2]);
+            } else {
+                this.word = split[0].substring(2);
+                aCount = Integer.parseInt(split[2]);
+            }
+        }
+
+        boolean matches(UsageCount candidate){
+            return this.word.equals(candidate.word);
+        }
+
+        void add(UsageCount count){
+            this.aCount += count.aCount;
+            this.anCount += count.anCount;
+        }
+
+        boolean before(UsageCount candidate) {
+            return candidate == null || this.word.compareTo(candidate.word) < 0;
+        }
+
+        @Override
+        public String toString(){
+            return word + "\t" + (anCount * 1.0 / (aCount + anCount)) + "\n";
+        }
+    }
+}
---
+++ learning/src/test/java/com/wabradshaw/plt/learning/determiners/data/AnDataSetBuilderTest.java
@@ -0,0 +1,239 @@
+package com.wabradshaw.plt.learning.determiners.data;
+
+import com.wabradshaw.ptl.learning.determiners.data.AnDataSetBuilder;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * A set of tests for the {@link com.wabradshaw.ptl.learning.determiners.data.AnDataSetBuilder}
+ */
+class AnDataSetBuilderTest {
+
+    private static final String EMPTY = "empty.txt";
+    private static final String SINGLE_A = "single_a.txt";
+    private static final String SINGLE_AN = "single_an.txt";
+    private static final String MULTIPLE_A = "multiple_a.txt";
+    private static final String MULTIPLE_AN = "multiple_an.txt";
+    private static final String CLASH_A = "clash_a.txt";
+    private static final String CLASH_AN = "clash_an.txt";
+    private static final String MULTI_CLASH_A = "multiclash_a.txt";
+    private static final String MULTI_CLASH_AN = "multiclash_an.txt";
+    private static final String BIG_A = "big_a.txt";
+    private static final String BIG_AN = "big_an.txt";
+
+    /**
+     * Tests that the builder can produce a single output file.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testCreateOneFile(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(SINGLE_A), load(SINGLE_AN), tempDir, 1);
+        assertEquals(1, tempDir.listFiles().length);
+        assertEquals("0.txt", getFile(tempDir,0).getName());
+    }
+
+    /**
+     * Tests that the builder can produce multiple output files.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testCreateMultipleFiles(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(SINGLE_A), load(SINGLE_AN), tempDir, 3);
+        assertEquals(3, tempDir.listFiles().length);
+        assertEquals("0.txt", getFile(tempDir,0).getName());
+        assertEquals("1.txt", getFile(tempDir,1).getName());
+        assertEquals("2.txt", getFile(tempDir,2).getName());
+    }
+
+    /**
+     * Tests that the builder can handle a single A entry.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testSingleA(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(SINGLE_A), load(EMPTY), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(1, content.size());
+        assertEquals("cat\t0.0", content.get(0));
+    }
+
+    /**
+     * Tests that the builder can handle a single An entry
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testSingleAn(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(EMPTY), load(SINGLE_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(1, content.size());
+        assertEquals("owl\t1.0", content.get(0));
+    }
+
+    /**
+     * Tests that the builder can handle a single A at the same time as a single An entry
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testSingleASingleAn(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(SINGLE_A), load(SINGLE_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(2, content.size());
+        assertEquals("cat\t0.0", content.get(0));
+        assertEquals("owl\t1.0", content.get(1));
+    }
+
+    /**
+     * Tests that the builder can handle multiple A entries.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testMultipleA(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(MULTIPLE_A), load(EMPTY), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(3, content.size());
+        assertEquals("cat\t0.0", content.get(0));
+        assertEquals("cow\t0.0", content.get(1));
+        assertEquals("zebra\t0.0", content.get(2));
+    }
+
+    /**
+     * Tests that the builder can handle multiple An entries
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testMultipleAn(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(EMPTY), load(MULTIPLE_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(3, content.size());
+        assertEquals("abacus\t1.0", content.get(0));
+        assertEquals("orange\t1.0", content.get(1));
+        assertEquals("ordinal direction\t1.0", content.get(2));
+    }
+
+    /**
+     * Tests that if a word appears in both a and an, the ratio will handle this.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testSingleClash(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(CLASH_A), load(CLASH_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(1, content.size());
+        assertEquals("herb\t0.4", content.get(0));
+    }
+
+    /**
+     * Tests that if a word appears several times in both a and an, the ratio will handle this.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testMulticlash(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(MULTI_CLASH_A), load(MULTI_CLASH_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(1, content.size());
+        assertEquals("herb\t0.7", content.get(0));
+    }
+
+    /**
+     * Tests that the builder can handle multiple distinct As and Ans
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testMultipleAMultipleAn(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(MULTIPLE_A), load(MULTIPLE_AN), tempDir, 1);
+        List<String> content =  getContent(tempDir, 0);
+        assertEquals(6, content.size());
+        assertEquals("abacus\t1.0", content.get(0));
+        assertEquals("cat\t0.0", content.get(1));
+        assertEquals("cow\t0.0", content.get(2));
+        assertEquals("orange\t1.0", content.get(3));
+        assertEquals("ordinal direction\t1.0", content.get(4));
+        assertEquals("zebra\t0.0", content.get(5));
+    }
+
+    /**
+     * Tests that reasonably large files don't break the system.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testBig(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(BIG_A), load(BIG_AN), tempDir, 1);
+        List<String> content = getContent(tempDir, 0);
+        assertEquals(819, content.size());
+    }
+
+    /**
+     * Tests that a result can be split across multiple output datasets. Each dataset should have at least a few items
+     * in (they'll tend to be about 20%, but I don't want rare failing tests due to chance). A and An should be
+     * interspersed.
+     *
+     * @param tempDir Temporary output directory.
+     */
+    @Test
+    void testMultipleDatasets(@TempDir File tempDir) throws IOException {
+        new AnDataSetBuilder().build(load(BIG_A), load(BIG_AN), tempDir, 5);
+
+        int total = 0;
+        for (int i = 0; i < 5; i++) {
+            List<String> content = getContent(tempDir, i);
+            assertTrue(content.size() > 20);
+
+            assertTrue(content.stream().anyMatch(c -> c.contains("\t1.0")));
+            assertTrue(content.stream().anyMatch(c -> c.contains("\t0.0")));
+            total += content.size();
+        }
+        assertEquals(819, total);
+    }
+
+    /**
+     * Creates a {@link File} representing the named resource in the determiners folder
+     *
+     * @param resource The name of the file
+     * @return The File object
+     */
+    private File load(String resource){
+        return new File(this.getClass().getResource("/determiners/" + resource).getPath());
+    }
+
+    /**
+     * Gets a dataset {@link File} from the temporary folder by index.
+     *
+     * @param tempDir The directory with the {@link File}
+     * @param i The index of the dataset
+     * @return The File object
+     */
+    private File getFile(File tempDir, int i){
+        return new File(tempDir.getPath() + "/" + i + ".txt");
+    }
+
+    /**
+     * Gets a list of all of the lines within the dataset file at a particular index
+     *
+     * @param tempDir The directory with the {@link File}
+     * @param i The index of the dataset
+     * @return The contents of the file
+     */
+    private List<String> getContent(File tempDir, int i) throws IOException {
+        return FileUtils.readLines(getFile(tempDir, i), Charset.defaultCharset());
+    }
+}
