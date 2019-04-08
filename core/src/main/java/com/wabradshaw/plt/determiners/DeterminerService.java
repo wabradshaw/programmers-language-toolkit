46d9b05a951295cd9d393c2a3b1d2d3f4f333a2b
William Bradshaw <w.a.bradshaw@gmail.com>
2019-04-08 17:10:05 +0100

    Stubs for the DeterminerService and the DeterminerServiceBuilder

---
+++ core/src/main/java/com/wabradshaw/plt/determiners/DeterminerService.java
@@ -0,0 +1,16 @@
+package com.wabradshaw.plt.determiners;
+
+/**
+ * <p>
+ * A DeterminerService is a class that provides methods for managing determiners. A determiner is a word that comes at
+ * the beginning of a noun phrase that tells the reader how specific the noun phrase is. For example: "<i>a</i> cat",
+ * "<i>an</i> apple", "<i>the</i> dog", "<i>some</i> food", or "<i>all</i> cheese".
+ * </p>
+ * <p>
+ * DeterminerServices should be created using a DeterminerServiceBuilder. These give you the ability to configure the
+ * service. Once built, all DeterminerService configuration is final.
+ * </p>
+ */
+public class DeterminerService {
+    
+}
