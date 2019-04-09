20511b17c22253e1e08d2f5912d51134fcbae77b
William Bradshaw <w.a.bradshaw@gmail.com>
2019-04-09 10:28:47 +0100

    Stub for the AANTrie

---
+++ core/src/main/java/com/wabradshaw/plt/determiners/AAnTrie.java
@@ -0,0 +1,13 @@
+package com.wabradshaw.plt.determiners;
+
+/**
+ * <p>
+ * An AAnTrie is a data structure representing whether the start of a word typically gets described using "a" or "an".
+ * AAnTries are arranged into a tree structure where each node represents a particular character of the string, and
+ * whether most words (up to that character) use "a" or "an". Each node contains further AAnTrie nodes for future
+ * letters.
+ * </p>
+ */
+class AAnTrie {
+
+}
